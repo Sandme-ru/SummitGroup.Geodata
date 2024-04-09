8a3438866db2608ee6ff9c34ba17566300259494
Shvyrkaloff <81988933+Shvyrkaloff@users.noreply.github.com>
2024-04-09 19:36:00 +0300

    Добавьте файлы проекта.

---
+++ SummitGroup.Geodata.Application/Entities/Address/Domain/Address.cs
@@ -0,0 +1,28 @@
+﻿namespace SummitGroup.Geodata.Application.Entities.Address.Domain;
+
+public class Address
+{
+    public string Source { get; set; }
+
+    public string Result { get; set; }
+
+    public string PostalCode { get; set; }
+
+    public string Country { get; set; }
+
+    public string Region { get; set; }
+
+    public string CityArea { get; set; }
+
+    public string CityDistrict { get; set; }
+
+    public string Street { get; set; }
+
+    public string House { get; set; }
+
+    public string GeoLat { get; set; }
+
+    public string GeoLon { get; set; }
+
+    public int QcGeo { get; set; }
+}
---
+++ SummitGroup.Geodata.Application/Entities/Address/Dto/AddressDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SummitGroup.Geodata.Application.Entities.Address.Dto;
+
+public class AddressDto
+{
+    public string Region { get; set; }
+
+    public string City { get; set; }
+
+    public string Street { get; set; }
+
+    public string House { get; set; }
+
+    public string Apartment { get; set; }
+
+}
