ac8bff3d19a42df54d7106bac9403f9de39160c9
Shvyrkaloff <81988933+Shvyrkaloff@users.noreply.github.com>
2024-04-10 13:27:07 +0300

    Fix headers

--- SummitGroup.Geodata.WebApi/Program.cs
+++ SummitGroup.Geodata.WebApi/Program.cs
@@ -2,6 +2,7 @@ using SummitGroup.Geodata.Application.Entities.Address.Interfaces;
 using SummitGroup.Geodata.Application.Entities.Address.Services;
 using SummitGroup.Geodata.Application.Entities.Location.Interfaces;
 using SummitGroup.Geodata.Application.Entities.Location.Services;
+using System.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,11 +20,15 @@ var nominatimUrl = builder.Configuration.GetSection("HttpClientSettings:Nominati
 builder.Services.AddHttpClient("Dadata", client =>
 {
     client.BaseAddress = new Uri(dadataUrl!);
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", builder.Configuration.GetSection("DataSettings:DadataToken").Value);
 });
+
 
 builder.Services.AddHttpClient("Nominatim", client =>
 {
     client.BaseAddress = new Uri(nominatimUrl!);
+    client.DefaultRequestHeaders.UserAgent.ParseAdd(builder.Configuration.GetSection("DataSettings:UserAgent").Value);
 });
 
 var app = builder.Build();
